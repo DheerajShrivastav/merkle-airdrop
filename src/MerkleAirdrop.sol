3c6f7111aa17db08217bfb38d6f3d43b86ab4ea6
Dheeraj Shrivastav <dheerajshrivastav98@gmail.com>
2025-07-11 16:33:01 +0530

    Improves Merkle airdrop claim process
    Adds claim validation to prevent duplicate claims.
    
    Updates the airdrop token to use IERC20 interface.
    Also, adds getter functions for merkle root and airdrop token.

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 ​
-import {ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 contract MerkleAirdrop {
@@ -12,11 +11,13 @@ contract MerkleAirdrop {
     using SafeERC20 for IERC20;
 
     error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
 
     event Claim(address indexed account, uint256 amount);
 
     bytes32 public immutable i_merkleRoot;
-    ERC20 private immutable i_airdropToken;
+    IERC20 private immutable i_airdropToken;
+    mapping (address cliamant => bool) private s_hasClaimed;
     constructor(bytes32 merkleRoot, address airdropToken) {
         i_merkleRoot = merkleRoot;
         i_airdropToken = airdropToken;
@@ -27,6 +28,10 @@ contract MerkleAirdrop {
         uint256 amount,
         bytes32[] calldata merkleProof
     ) external {
+        if (s_hasClaimed[account]) {
+            revert MerkleAirdrop__AlreadyClaimed();
+            
+        }
         bytes32 leaf = keccak256(bytes.concat(
             keccak256(abi.encode(account, amount));
         ));
@@ -34,8 +39,17 @@ contract MerkleAirdrop {
             revert MerkleAirdrop__InvalidProof();
         }
 
-        
+
+        s_hasClaimed[account] = true;
         emit Claim(account, amount);
         i_airdropToken.safeTransfer(account, amount);
+    }
+
+    function getMerkleRoot() external view returns (bytes32) {
+        return i_merkleRoot;
+    }
+
+    getAirdropToken() external view returns (IERC20) {
+        return i_airdropToken;
     }
 }
