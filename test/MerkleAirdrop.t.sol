46a362bd124b9a8c44cebe47554a78a488337114
Dheeraj Shrivastav <dheerajshrivastav98@gmail.com>
2025-07-12 16:57:21 +0530

    Refactor MerkleAirdrop test to include proof array and enhance claim functionality

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -10,15 +10,22 @@ contract MerkleAirdropTest is Test {
 
     bytes32 public ROOT =
         0xf30d5b84fa4d98e15975269eff95a775a609d4a4402251b99426461050704ad8;
+
     uint256 public AMOUNT_TO_CLAIM = 25 * 1e18;
     uint256 public AMOUNT_TO_SEND;
 
     address user;
     uint256 userPrivKey;
 
-    bytes32 proofOne;
-    bytes32 proofTwo;
-    bytes32[2] public PROOF;
+    // bytes32 proofOne;
+    // bytes32 proofTwo;
+    // bytes32[2] public PROOF;
+
+    bytes32 proofOne =
+        0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 proofTwo =
+        0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] public PROOF = [proofOne, proofTwo];
 
     function setUp() public {
         token = new BagelToken();
@@ -37,5 +44,17 @@ contract MerkleAirdropTest is Test {
 
     function testUsersCanClaim() public {
         console.log("User address: %s", user);
+        uint256 startingBalance = token.balanceOf(user);
+        vm.prank(user);
+
+        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+        uint256 endingBalance = token.balanceOf(user);
+
+        console.log("User's Ending Balance: ", endingBalance);
+        assertEq(
+            endingBalance - startingBalance,
+            AMOUNT_TO_CLAIM,
+            "User did not receive the correct amount of tokens"
+        );
     }
 }
